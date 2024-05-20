7859740d1eb1a85e86cb5a7331d83dd91e51fa89
yanrodrigues205 <yan.pablo205@gmail.com>
2024-05-20 15:43:19 -0300

    :sparkles: feat: change of collection point

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -21,7 +21,7 @@ function getAll()
             response[i].description,
             response[i].work_hours,
             response[i].collect_user,
-            `<button type="button" data-id="${response[i].id}" class="btn btn-success"><i class="fa-solid fa-pencil"></i></button> <button type="button" data-id="${response[i].id}" onclick="actionDeleteById(this)" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal" ><i class="fa-solid fa-trash"></i></button>`
+            `<button type="button" data-id="${response[i].id}" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="actionUpdateById(this)"><i class="fa-solid fa-pencil"></i></button> <button type="button" data-id="${response[i].id}" onclick="actionDeleteById(this)" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal" ><i class="fa-solid fa-trash"></i></button>`
         ]).draw();
       }
    })
@@ -55,6 +55,58 @@ function deleteById(element)
             getAll();
         }
     })
+}
+
+function actionUpdateById(element)
+{
+    let id = element.getAttribute("data-id");
+    console.log(id)
+    $.ajax({
+        url: dominio + "collect_point/getbyid",
+        dataType: "json",
+        type: "POST",
+        data: JSON.stringify({ id: id }),
+        contentType: "application/json; charset=utf-8"
+    }).done(response => {
+        console.log(response.name);
+        $("#name_cp").val(response.name);
+        $("#description_cp").val(response.description);
+        $("#id_cp").val(id);
+        $("#work_hours_cp").val(response.work_hours);
+        $("#collect_user_cp").val(response.collect_user);
+
+    });
+}
+
+function updateById()
+{
+    const id = $("#id_cp").val();
+    const name = $("#name_cp").val();
+    const description = $("#description_cp").val();
+    const work_hours = $("#work_hours_cp").val();
+    const collect_user = $("#collect_user_cp").val();
+
+    let data = {
+        id,
+        name,
+        description,
+        work_hours,
+        collect_user
+    }
+
+
+
+    $.ajax({
+        url: dominio + "collect_point/update",
+        dataType: "json",
+        type: "PUT",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8"
+    }).done(response => {
+        alert(response.message);
+        getAll();
+    });
+
 }
 
 
