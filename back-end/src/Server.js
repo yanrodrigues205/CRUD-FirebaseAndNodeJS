9b19f8a71038e21ea19cfc2e8c9bd4423e078231
yanrodrigues205 <yan.pablo205@gmail.com>
2024-05-20 14:20:59 -0300

    :wrench: chore: cleaning import and unnecessary variables

--- back-end/src/Server.js
+++ back-end/src/Server.js
@@ -3,23 +3,18 @@ import bodyParser from "body-parser";
 import admin from "firebase-admin";
 import router from "./Routes.js";
 import cors from "cors";
-import serviceAccount from "./serviceAccount.json" assert { type: "json" };
 
 class Server
 {
     #router;
     #bodyParser;
     #app;
-    #admin;
-    #database;
 
     constructor()
     {
         this.#router = router;
         this.#bodyParser = bodyParser;
         this.#app = express();
-        this.#admin = admin;
-        this.#database = false;
     }
 
     async __init__()
